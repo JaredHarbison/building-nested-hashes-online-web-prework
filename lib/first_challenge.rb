525f8b067ca57868aa3044a85f3dbbc0d81eedfb
Jared Harbison <jared.harbison@gmail.com>
2019-01-09 21:03:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
   epic_tragedy = {
-    :montague => {:patriarch :matriarch :hero_friends:},
+    :montague => {:patriarch =>:matriarch :hero_friends:},
     :capulet => {:patriarch :matriarch :hero :heroine_friends}
   }
 end
